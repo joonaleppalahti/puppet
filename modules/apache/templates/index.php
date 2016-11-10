3c343a8b05f3ebb680e07ae68efe79ffd7600034
joonaleppalahti <joona.leppalahti@myy.haaga-helia.fi>
2016-11-10 15:31:02 +0200

    added index.php, removed old apache2 module

---
+++ modules/apache/templates/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+	<title>Hello World!</title>
+	<meta charset="utf-8" />
+</head>
+<body>
+	<h2>Hello World!</h2>
+	<p><?php print"Testing PHP"; ?></p>
+</body>
+</html>

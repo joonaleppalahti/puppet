162e019c79d9072faca87004de1ef4435d684a8d
joonaleppalahti <joona.leppalahti@myy.haaga-helia.fi>
2016-11-10 21:02:32 +0200

    added allowcdrom to mysql

--- modules/mysql/manifests/server/install.pp
+++ modules/mysql/manifests/server/install.pp
@@ -4,6 +4,7 @@ class mysql::server::install {
   if $mysql::server::package_manage {
 
     package { 'mysql-server':
+	allowcdrom => "true",
       ensure          => $mysql::server::package_ensure,
       install_options => $mysql::server::install_options,
       name            => $mysql::server::package_name,
